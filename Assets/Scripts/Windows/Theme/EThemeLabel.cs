7243a8627dd01ddcd9d937af6c86360f5442884d
Val Knight <val.knight@unity3d.com>
2025-01-26 12:49:27 +0000

    theme

---
+++ Assets/Scripts/Windows/Theme/EThemeLabel.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Windows.Theme
+{
+    public class EThemeLabel: MonoBehaviour
+    {
+        public TMP_Text text;
+        public Button button;
+    }
+}
